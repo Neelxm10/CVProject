a4aeaeff4d38ff24016545b6c81d827b2aeb5d24
Neelxm10 <mukh0023@algonquinlive.com>
2024-02-08 00:46:53 -0500

    Converted image to binary image and added masks

--- imageProcessor.py
+++ imageProcessor.py
@@ -9,13 +9,19 @@ def imageProcessor(img, framecnt):
     print('processing image...\n')
     #show the original image
     cv.imshow('video reader', img)
-    cv.imwrite('Frame_Dump/Frame'+str(framecnt)+ '.png', img)
+    #cv.imwrite('Frame_Dump/Frame'+str(framecnt)+ '.png', img)
     #Convert to HSV and display HSV image frame by frame. 
     imgHSV= cv.cvtColor(img, cv.COLOR_BGR2HSV)
     imgHSVGS = cv.cvtColor(imgHSV, cv.COLOR_BGR2GRAY)
-    cv.imshow('HSV image', imgHSVGS)
-    cv.imwrite('Frame_Dump/FrameHSV'+str(framecnt)+ '.png', imgHSV)
+    imgSat= imgHSV[:,:,1]
+    imgVal = imgHSV[:,:,0]
+    ret, mask = cv.threshold(imgVal, 75, 200, cv.THRESH_BINARY)
+    disc = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7))
+    eroded_mask = cv.erode(mask, disc)
+    dilated_mask = cv.dilate(eroded_mask, disc)
+    cv.imshow('Saturation Grayscale', mask)
+    cv.imshow('Eroded masked image', eroded_mask)
+    cv.imshow('Dilated Masked image', dilated_mask)
     cv.waitKey(20)
-    
     
 cv.destroyAllWindows()
--- videoreader.py
+++ videoreader.py
@@ -11,16 +11,24 @@ capture = cv.VideoCapture(filename)
 
 #frame counter initialization
 framecnt = 0
-os.mkdir('Frame_Dump')
+#make a directory called frame dump
+if not os.path.exists:
+    os.mkdir('Frame_Dump')
+else:
+    print("Directory already exists, proceeding with overwriting directory\n")
+#while the video capture bit is true
 while (capture.isOpened()):
+    #store the state whether there is a frame or not, and record the frame data
     state,frame = capture.read()
+    #if state is true (state == 1), process the image
     if state:
         framecnt+=1
         imageProcessor(frame,framecnt)
-        print('Print....FrameCount:'+str(framecnt)+'\n')
+        print('FrameCount:'+str(framecnt)+'\n')
         
-      
+    #other wise end the program (if there is no frame, state == 0)  
     else:
+        print("No remaining frames to process\n") 
         break
 
 
