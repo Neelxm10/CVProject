ecf7cbf514ceab63e9dc4aadbf2fffd1bc8af10a
Neelxm10 <mukh0023@algonquinlive.com>
2024-02-08 03:09:27 -0500

    trying line detection, picking up vertices, and tracking

--- imageProcessor.py
+++ imageProcessor.py
@@ -16,21 +16,42 @@ def imageProcessor(img, framecnt):
     imgVal = imgHSV[:,:,0]
     imgHue = imgHSV[:,:,2]
     #Apply initial binary mask using Saturation image channel
-    ret, mask = cv.threshold(imgSat, 0, 255, cv.THRESH_BINARY)
+    ret, mask = cv.threshold(imgVal, 90, 220, cv.THRESH_BINARY)
 
     #create structuring element and use it to perform opening mask
     disc = cv.getStructuringElement(cv.MORPH_ELLIPSE, (7,7))
-    eroded_mask = cv.erode(mask, disc)
-    dilated_mask = cv.dilate(eroded_mask, disc)
-    #Subtract dilated mask from initial binary mask to retreive only the edges
-    edges = cv.absdiff(mask, dilated_mask)
     
-    #Uncomment these for when you wanna check out the masks.
-    #edges = cv.Canny(dilated_mask, 120,180)
-    #cv.imshow('Saturation Grayscale', mask)
-    #cv.imshow('Dilated Masked image', dilated_mask)
+    dilated_mask = cv.dilate(mask, disc)
+    eroded_mask = cv.erode(dilated_mask, disc)
+    #Subtract dilated mask from initial binary mask to retreive only the edges
+    edges = cv.absdiff(mask, eroded_mask)
+    lines = cv.HoughLines(edges, 1, np.deg2rad(1), 0)
+
+
+    # Draw lines on a copy of the original image
+     # Create an empty image to draw lines
+    img_with_lines = np.zeros_like(img)
+
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv.line(img_with_lines, (x1, y1), (x2, y2), (0, 90, 255), 2)
+
+    # Display images
+    #v.imshow('Original Image', img)
     cv.imshow('Edge Detection', edges)
-    cv.imwrite('Frame_Dump/Frame_'+str(framecnt)+'.png',edges)
-    cv.waitKey(20)
+    cv.imshow('Image with Lines', img_with_lines)
+    cv.imwrite('Frame_Dump/Frame_' + str(framecnt) + '.png', img_with_lines)
+    cv.waitKey(0)
+    #With Edge detection performed now we get the contour lines
+
     
 cv.destroyAllWindows()
