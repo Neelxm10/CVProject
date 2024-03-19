505cf99490bca431fb2c4f8e215d62ba4b2f7ef0
Neelxm10 <mukh0023@algonquinlive.com>
2024-03-18 22:28:18 -0400

    combined rotation matrix and translation vector for each sampled frame

--- perspectiveCalib.py
+++ perspectiveCalib.py
@@ -7,8 +7,22 @@ savedir = 'calibration_data/'
 cam_mtx = np.load(savedir+'optimalIntrinsic.npy')
 rvecs = np.load(savedir+'rvecs.npy')
 tvecs = np.load(savedir+'tvecs.npy')
+#print(rvecs)
 
+# Initialize an identity matrix to accumulate rotations
+combined_rotation_matrix = np.eye(3)
+print(combined_rotation_matrix)
 
-
-RotMtx, _ = cv.Rodrigues(rvecs[0])
-print(RotMtx)
+# Initialize a zero vector to accumulate translations
+combined_translation_vector = np.zeros((1,3, 1))
+# Loop through each rotation vector
+for rvec, tvec in zip(rvecs, tvecs):
+    # Convert rotation vector to rotation matrix using Rodrigues formula
+    rotation_matrix, _ = cv.Rodrigues(rvec)
+    # Accumulate rotations by multiplying rotation matrices
+    combined_rotation_matrix = np.dot(combined_rotation_matrix, rotation_matrix)
+    combined_translation_vector += tvec
+print("combined rotation matrix from all sample frames")
+print(combined_rotation_matrix)
+print("combined translation vector")
+print(combined_translation_vector)
